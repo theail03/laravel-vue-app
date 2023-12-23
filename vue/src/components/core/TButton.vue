89cb79ff95191ffcb0efec312e36f8c668b6d569
theail03 <andresirias03@gmail.com>
2023-12-22 23:16:03 -0600

    Refactor TButton file

--- vue/src/components/core/TButton.vue
+++ vue/src/components/core/TButton.vue
@@ -31,87 +31,40 @@ const props = defineProps({
   },
 });
 
-let classes = ref([
-  "flex",
-  "text-sm",
-  "border",
-  "border-2",
-  "border-transparent",
-]);
+let classes = ref(["flex", "text-sm", "border", "border-2", "border-transparent"]);
 
 if (props.link) {
-  classes.value = [
-    ...classes.value,
-    "transition-colors",
-  ];
+  classes.value = [...classes.value, "transition-colors"];
 
   switch (props.color) {
     case "indigo":
-      classes.value = [
-        ...classes.value,
-        "text-indigo-500",
-        "focus:border-indigo-500",
-      ];
+      classes.value = [...classes.value, "text-indigo-500", "focus:border-indigo-500"];
       break;
     case "red":
       classes.value = [...classes.value, "text-red-500", "focus:border-red-500"];
   }
 
 } else {
-  classes.value = [
-    ...classes.value,
-    "text-white",
-    "focus:ring-2",
-    "focus:ring-offset-2",
-  ];
+  classes.value = [...classes.value, "text-white", "focus:ring-2", "focus:ring-offset-2"];
 
   switch (props.color) {
     case "indigo":
-      classes.value = [
-        ...classes.value,
-        "bg-indigo-600",
-        "hover:bg-indigo-700",
-        "focus:ring-indigo-500",
-      ];
+      classes.value = [...classes.value, "bg-indigo-600", "hover:bg-indigo-700", "focus:ring-indigo-500"];
       break;
     case "red":
-      classes.value = [
-        ...classes.value,
-        "bg-red-600",
-        "hover:bg-red-700",
-        "focus:ring-red-500",
-      ];
+      classes.value = [...classes.value, "bg-red-600", "hover:bg-red-700", "focus:ring-red-500"];
       break;
     case "green":
-      classes.value = [
-        ...classes.value,
-        "bg-emerald-500",
-        "hover:bg-emerald-600",
-        "focus:ring-emerald-400",
-      ];
+      classes.value = [...classes.value, "bg-emerald-500", "hover:bg-emerald-600", "focus:ring-emerald-400"];
       break;
 
   }
 }
 
 if (props.circle) {
-  classes.value = [
-    ...classes.value,
-    "h-8",
-    "w-8",
-    "items-center",
-    "justify-center",
-    "rounded-full",
-    "text-sm",
-  ];
+  classes.value = [...classes.value, "h-8", "w-8", "items-center", "justify-center", "rounded-full", "text-sm"];
 } else {
-  classes.value = [
-    ...classes.value,
-    "p-0",
-    "py-2",
-    "px-4",
-    "rounded-md"
-  ];
+  classes.value = [...classes.value, "p-0", "py-2", "px-4", "rounded-md"];
 }
 </script>
 
