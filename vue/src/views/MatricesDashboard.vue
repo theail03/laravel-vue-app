0c4ea63874c00ffdbb415e37a0b617b7c34207f2
theail03 <andresirias03@gmail.com>
2024-01-15 13:41:46 -0600

    Add top margin to 'Edit Matrix' and 'View Matrix' buttons

--- vue/src/views/MatricesDashboard.vue
+++ vue/src/views/MatricesDashboard.vue
@@ -32,7 +32,7 @@
           </div>
           <h3 class="font-bold text-xl mb-3">{{ latestMatrix.title }}</h3>
           <MatrixInfo :matrix="latestMatrix" />
-          <div class="flex justify-between">
+          <div class="flex justify-between mt-3">
             <TButton :to="{ name: 'MatrixEdit', params: { id: latestMatrix.id } }" link>
               <PencilIcon class="w-5 h-5 mr-2" />
               Edit Matrix
