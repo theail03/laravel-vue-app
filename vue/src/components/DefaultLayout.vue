b5d3eb3a4c8f8e89fad47077e03c29c197e7a8f1
theail03 <andresirias03@gmail.com>
2024-01-15 17:51:28 -0600

    Change logo

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -16,7 +16,7 @@
             <div class="flex-shrink-0">
               <img
                 class="h-8 w-8"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                src="../assets/logo.png"
                 alt="Workflow"
               />
             </div>
