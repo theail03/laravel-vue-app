86d508dbd96db2f1ae9a0a8d1adc38fbe05d3e3c
theail03 <andresirias03@gmail.com>
2024-01-15 20:49:40 -0600

    Change logo in login and register pages

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,7 +2,7 @@
   <div>
     <img
       class="mx-auto h-12 w-auto"
-      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+      src="../assets/logo.png"
       alt="Workflow"
     />
     <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
