a1cd6066a45e1a5ce9d287da07e4caf1daf38441
theail03 <andresirias03@gmail.com>
2024-03-06 23:09:58 -0600

    Add Sign in with Google button to layout

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -38,10 +38,12 @@
               </div>
             </div>
           </div>
+
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
+
               <!-- Profile dropdown -->
-              <Menu as="div" class="ml-3 relative">
+              <Menu v-if="isAuthenticated" as="div" class="ml-3 relative">
                 <div class="flex">
                   <MenuButton
                     class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
@@ -99,8 +101,23 @@
                   </MenuItems>
                 </transition>
               </Menu>
+
+              <!-- "Sign in with Google" button, shown if not authenticated -->
+              <button v-else
+                @click="console.log('Sign in with Google')"
+                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-flex items-center shadow-lg"
+              >
+                <svg class="w-6 h-6 mr-2" aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 48 48">
+                  <path fill="#FFC107" d="M43.6 20H24v8h11.8C34.7 36.3 29.8 40 24 40c-8.4 0-15.6-6.8-16-15.2h-8v-6c0-8.8 7.2-16 16-16 4.4 0 8.4 1.8 11.3 4.7l5.7-5.7C34.6 4.1 29.6 2 24 2 10.8 2 0 12.8 0 26s10.8 24 24 24c13.3 0 24-10.7 24-24 0-1.3-.2-2.7-.4-4z"/>
+                  <path fill="#FF3D00" d="M6.3 29.8C5.5 27.9 5 25.8 5 23.8s.5-4.1 1.3-6V11h8c1.1 4.5 4.5 8.2 9.7 9.8v9.2H15.6c-1.5 0-2.9-.4-4.3-1.2z"/>
+                  <path fill="#4CAF50" d="M24 42c4.5 0 8.5-1.6 11.6-4.2l-5.4-5.4c-1.6 1.1-3.6 1.8-6.2 1.8-5.8 0-10.7-3.7-12.4-8.8H6.2v6C10.4 38.2 16.6 42 24 42z"/>
+                  <path fill="#1976D2" d="M43.6 20H24v8h11.8C35.9 28 34.7 30 33.2 32l5.4 5.4C41.5 34.4 43.6 30 43.6 26c0-1.3-.2-2.6-.4-4z"/>
+                </svg>
+                Sign in with Google
+              </button>
             </div>
           </div>
+
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
             <DisclosureButton
@@ -236,6 +253,7 @@ export default {
       user: computed(() => store.state.user.data),
       navigation,
       logout,
+      isAuthenticated,
     };
   },
 };
