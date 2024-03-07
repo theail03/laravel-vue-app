17d2d285e98a2b1cfee19ba8b6c80a9675ee28e4
theail03 <andresirias03@gmail.com>
2024-03-07 15:03:11 -0600

    Add functionality to Sign in with Google button

--- vue/src/components/core/GoogleSignInButton.vue
+++ vue/src/components/core/GoogleSignInButton.vue
@@ -1,5 +1,10 @@
 <template>
-    <button>
-        <img src="../../assets/btn_google_signin_dark_normal_web.png" alt="Sign in with Google" />
-    </button>
+  <a :href="googleAuthUrl">
+    <img src="../../assets/btn_google_signin_dark_normal_web.png" alt="Sign in with Google" />
+  </a>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const googleAuthUrl = ref(`${import.meta.env.VITE_API_BASE_URL}/google-auth/redirect`);
+</script>
